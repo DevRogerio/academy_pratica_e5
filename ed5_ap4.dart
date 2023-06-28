199a2c4707a2a6202501144c91ae46609cfdf265
DevRogerio <130984908+DevRogerio@users.noreply.github.com>
2023-06-28 14:35:31 -0300

    ed5_ap4.dart

---
+++ ed5_ap4.dart
@@ -0,0 +1,31 @@
+v# academy_pratica_e5
+
+void main() {
+  final listaNomes = [
+    "Joao",
+    "Maria",
+    "Pedro",
+    "Maria",
+    "Ana",
+    "Joao",
+    "Maria",
+    "Fernanda",
+    "Carlos",
+    "Maria"
+  ];
+
+  final nome = 'Joao';
+  final quantidade = contarNome(listaNomes, nome);
+
+  if (quantidade == 1) {
+    print('O nome $nome foi encontrado 1 vez');
+  } else if (quantidade > 0) {
+    print('O nome $nome foi encontrado $quantidade vezes');
+  } else {
+    print('O nome não foi encontrado');
+  }
+}
+
+int contarNome(List<String> listaNomes, String nome) {
+  return listaNomes.where((element) => element == nome).length;
+}
